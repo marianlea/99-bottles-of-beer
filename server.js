7a7ce85bdc82768f7223f6b1df94fd96e8a64beb
Lea Baldevieso <mlbaldevieso@gmail.com>
2024-03-24 14:26:16 +1100

    mvp

--- server.js
+++ server.js
@@ -1,43 +1,54 @@
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
-const app = express()
-const port = 8080
-const player = require('play-sound')(opts = {})
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+const port = 8080;
+const player = require("play-sound")((opts = {}));
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.use(express.static('public'))
-app.use(expressLayouts)
+app.use(express.static("public"));
+app.use(expressLayouts);
 
+app.get("/", (req, res) => {
+  let numOfBeer = 99;
+  let oneLessBeer = numOfBeer - 1;
+  res.render("home", { beerNumber: numOfBeer, oneLessBeer: oneLessBeer });
+});
 
-
-app.get('/', (req, res) => {
-    // console.log(req);
-    let numOfBeer = 99
-    let oneLessBeer = numOfBeer - 1
-    res.render('home', { beerNumber: numOfBeer, oneLessBeer : oneLessBeer})
-})
-
-app.get('/:beerNum', (req, res) => {
-    // console.log(req);
-    player.play('./audio/drinking-beer.mp3', (err) => {
-        if (err) {
-            console.log(err);
-        }
-    })
-    let beerNum = req.params.beerNum
-    // console.log(beerNum);
-    let numOfBeer = Number(beerNum)
-    numOfBeer -= 1
-    if (beerNum > '1') {
-        res.render('beerNum', { beerCount: beerNum, oneLessBeer: numOfBeer, bottles: 'bottles', clickHere: 'Take one down.'})
-    } else if (beerNum === '1') {
-        res.render('beerNum', { beerCount: beerNum, oneLessBeer: numOfBeer, bottles: 'bottle', clickHere: 'Take one down.'})
-    } else if (beerNum === '0') {
-        res.render('beerNum', { beerCount: 'No', oneLessBeer: '', bottles: 'bottles', clickHere: 'Start over.' })
+app.get("/:beerNum", (req, res) => {
+  player.play("./audio/drinking-beer.mp3", (err) => {
+    if (err) {
+      console.log(err);
     }
-})
+  });
+  let beerNum = req.params.beerNum;
+
+  let numOfBeer = Number(beerNum);
+  numOfBeer -= 1;
+  if (beerNum > "1") {
+    res.render("beerNum", {
+      beerCount: beerNum,
+      oneLessBeer: numOfBeer,
+      bottles: "bottles",
+      clickHere: "Take one down.",
+    });
+  } else if (beerNum === "1") {
+    res.render("beerNum", {
+      beerCount: beerNum,
+      oneLessBeer: numOfBeer,
+      bottles: "bottle",
+      clickHere: "Take one down.",
+    });
+  } else if (beerNum === "0") {
+    res.render("beerNum", {
+      beerCount: "No",
+      oneLessBeer: "",
+      bottles: "bottles",
+      clickHere: "Start over.",
+    });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`server listening on port ${port}`);
-})
+  console.log(`server listening on port ${port}`);
+});
